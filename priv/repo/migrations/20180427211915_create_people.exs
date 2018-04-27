60ea8aa0b327c0688d985529899e9c810db9685a
Karen Pacheco <pachecovejar.k@gmail.com>
2018-04-27 20:43:25 -0300

    added migration

--- priv/repo/migrations/20180427211915_create_people.exs
+++ priv/repo/migrations/20180427211915_create_people.exs
@@ -2,8 +2,10 @@ defmodule Friends.Repo.Migrations.CreatePeople do
   use Ecto.Migration
 
   def change do
-    add :first_name, :string
-    add :last_name, :string
-    add :age, :integer
+    create table(:people) do
+      add :first_name, :string
+      add :last_name, :string
+      add :age, :integer
+    end
   end
 end
